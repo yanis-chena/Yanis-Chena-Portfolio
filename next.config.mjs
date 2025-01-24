48d25199023cf1b9e38d7d605ab52b7196005286
forrrix <forrix@gmx.fr>
2025-01-24 16:48:24 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig; // Remove withSentryConfig
+export default nextConfig;
